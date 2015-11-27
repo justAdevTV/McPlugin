e2014887e478ba55b9abef65d046592a9ec0b43d
Justin Punzalan <justpunz@Live.com>
2015-11-26 17:07:59 -0800

    Created onEnable, onDisable & pluginManager

--- src/com/rhota/mcplugin/Core.java
+++ src/com/rhota/mcplugin/Core.java
@@ -1,5 +1,24 @@
 package com.rhota.mcplugin;
 
-public class Core {
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Created by justAdevTV on 11/26/2015.
+ */
+
+public class Core extends JavaPlugin{
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	 public void registerListeners(){
+		 PluginManager pluginManager = getServer().getPluginManager();
+	 }
+	
 }
