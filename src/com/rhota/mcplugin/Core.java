448732faa0c96503a7d0429dc91c586e51228ac6
Justin Punzalan <justpunz@Live.com>
2015-11-27 16:19:03 -0800

    Removed Comments

--- src/com/rhota/mcplugin/Core.java
+++ src/com/rhota/mcplugin/Core.java
@@ -14,24 +14,9 @@ public class Core extends JavaPlugin {
 	
 	public void onEnable() {
 		registerListeners = new RegisterListener(this);
-		//registerListenrs();
 	}
 
 	public void onDisable() {
 
 	}
-
-	/*
-	public void registerListenrs(){
-		PluginManager pluginManager = getServer().getPluginManager();
-		// TODO Add toggle
-		pluginManager.registerEvents(new BlockListeners(), this);
-		pluginManager.registerEvents(new DamageListener(), this);
-		pluginManager.registerEvents(new ExplosionListener(), this);
-		pluginManager.registerEvents(new FoodListener(), this);
-		pluginManager.registerEvents(new ItemListeners(), this);
-		pluginManager.registerEvents(new WeatherListener(), this);
-		pluginManager.registerEvents(new AchievementListener(), this);
-	}*/
-	
 }
