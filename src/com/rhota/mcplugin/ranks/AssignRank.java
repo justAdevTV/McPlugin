a95fb10862319034627f2c1eda549b7f3ce8582c
Joey Chorbajian <joey.chorbajian@gmail.com>
2015-11-27 17:56:26 -0800

    Added Assign Rank

---
+++ src/com/rhota/mcplugin/ranks/AssignRank.java
@@ -0,0 +1,16 @@
+package com.rhota.mcplugin.ranks;
+
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+
+public class AssignRank {
+	private Plugin p;
+	public AssignRank(Plugin p) {
+		this.p = p;
+	}
+	
+	public void setRank(Player p, Rank r) {
+		p.setMetadata("rank", new FixedMetadataValue(this.p, r));
+	}
+}
